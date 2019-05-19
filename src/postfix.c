8d9b40d26548cd076dc3e0507c912d6d0a55dc4d
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-05-19 20:25:13 -0300

    feat: add support to priorities

--- src/postfix.c
+++ src/postfix.c
@@ -37,6 +37,23 @@ is_operand(int operand)
 }
 
 /**
+ * @brief: Check the priority of the given operator.
+ */
+int
+priority(int operator)
+{
+  switch (operator) {
+    case '(': return 0;
+    case '+':
+    case '-': return 1;
+    case '*':
+    case '/': return 2;
+  }
+
+  return -1;
+}
+
+/**
  * @brief: Encode an expression to a postfix one.
  */
 char*
@@ -52,18 +69,37 @@ postfix_encode(char* expression)
     if (is_operand(expression[i]))
       postfix[len_postfix++] = expression[i];
 
-    /* For an operator, push it into operators stack */
-    else if (is_operator(expression[i]))
+    /* For an operator, while there is another operator
+     * with higher or equal priority in the stack, we must
+     * pop it and append onto the expression, to only then
+     * push the current operator into the operators stack.
+     */
+    else if (is_operator(expression[i])) {
+      while (!stack_is_empty(&operators) && priority(stack_top(&operators)) >= priority(expression[i]))
+        postfix[len_postfix++] = stack_pop(&operators);
+      stack_push(&operators, expression[i]);
+    }
+
+    /* For an opening round bracket, push it into stack - as it has
+     * priority 0
+     */
+    else if (expression[i] == '(')
       stack_push(&operators, expression[i]);
 
-    /* For an opening round bracket, just ignore it */
-    else if (expression[i] == '(')
-      continue;
-
-    /* For a closing round bracket, add last operator onto postfix */
-    else if (expression[i] == ')')
-      postfix[len_postfix++] = stack_pop(&operators);
+    /* For a closing round bracket, pop all operators from
+     * stack until an opening round bracket is found. Then,
+     * pop the opening round bracket and discard it.
+     */
+    else if (expression[i] == ')') {
+      while (stack_top(&operators) != '(')
+        postfix[len_postfix++] = stack_pop(&operators);
+      stack_pop(&operators);
+    }
   }
+
+  /* Pop anything left in operators stack */
+  while (!stack_is_empty(&operators))
+    postfix[len_postfix++] = stack_pop(&operators);
 
   /* Finish postfix string with a null char */
   postfix[len_postfix] = '\0';
--- test/postfix-test.c
+++ test/postfix-test.c
@@ -5,8 +5,16 @@
 void
 test_postfix_encode()
 {
-  char* expected = postfix_encode("((2*6)+(1/4))");
-  assert_string("26*14/+", expected, "Expression completely surrounded by () without spaces");
+  char* actual;
+
+  actual = postfix_encode("((2*6)+(1/4))");
+  assert_string("26*14/+", actual, "Expression completely surrounded by () without spaces");
+
+  actual = postfix_encode("2+6*4-9");
+  assert_string("264*+9-", actual, "Expression without () nor spaces and with priorities");
+
+  actual = postfix_encode("(2+8)*9");
+  assert_string("28+9*", actual, "Expression with ()");
 }
 
 int
