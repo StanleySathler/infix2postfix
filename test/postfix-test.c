17dafeb68cc5334cd504e379a3dd06cb7488dd8d
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-05-20 20:35:57 -0300

    chore(tests): add test with spaces

--- test/postfix-test.c
+++ test/postfix-test.c
@@ -8,13 +8,16 @@ test_postfix_encode()
   char* actual;
 
   actual = postfix_encode("((2*6)+(1/4))");
-  assert_string("26*14/+", actual, "Expression completely surrounded by () without spaces");
+  assert_string("26*14/+", actual, "Expression completely surrounded by ()");
 
   actual = postfix_encode("2+6*4-9");
-  assert_string("264*+9-", actual, "Expression without () nor spaces and with priorities");
+  assert_string("264*+9-", actual, "Expression with different priorities");
 
   actual = postfix_encode("(2+8)*9");
   assert_string("28+9*", actual, "Expression with ()");
+
+  actual = postfix_encode("(2 + 9)*12+(5/1)");
+  assert_string("29+12*51/+", actual, "Expression with spaces");
 }
 
 int
